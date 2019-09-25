8ba5cf8478e533eb80eafdffa06db3576acf4f15
Ryan <ryanc1934@gmail.com>
2019-09-25 19:39:38 -0400

    Removed unused test.cpp. Added unit tests for PID controller to pid_test.cpp.

---
+++ test/pid_test.cpp
@@ -0,0 +1,62 @@
+/**
+ * @author Ryan Cunningham
+ * @author Pablo Sanhueza
+ * @file pid_test.cpp
+ * @brief Unit Tests for PID Controller
+ * @copyright 2019
+ */
+
+#include <gtest/gtest.h>
+#include "pid.hpp"
+
+/**
+ * Tests constructor for PID controller.
+ */
+TEST(PIDTest, constructor) {
+  PID pid = PID(1, 2, 3, 4, 5);
+
+  EXPECT_DOUBLE_EQ(1, pid.getKp());
+  EXPECT_DOUBLE_EQ(2, pid.getKi());
+  EXPECT_DOUBLE_EQ(3, pid.getKd());
+  EXPECT_DOUBLE_EQ(4, pid.getDt());
+  EXPECT_DOUBLE_EQ(5, pid.getSetpoint());
+}
+
+/**
+ * Tests the proportional control part of the PID controller independently.
+ */
+TEST(PIDTest, compute_proportional) {
+  PID pid = PID(2, 0, 0, 1, 0);
+
+  pid.compute(1);
+  pid.compute(2);
+  double control = pid.compute(3);
+
+  EXPECT_DOUBLE_EQ(-6, control);
+}
+
+/**
+ * Tests the integral control part of the PID controller independently.
+ */
+TEST(PIDTest, compute_integral) {
+  PID pid = PID(0, 2, 0, 1, 0);
+
+  pid.compute(1);
+  pid.compute(2);
+  double control = pid.compute(3);
+
+  EXPECT_DOUBLE_EQ(-12, control);
+}
+
+/**
+ * Tests the derivative control part of the PID controller independently.
+ */
+TEST(PIDTest, compute_derivative) {
+  PID pid = PID(0, 0, 2, 1, 0);
+
+  pid.compute(1);
+  pid.compute(2);
+  double control = pid.compute(3);
+
+  EXPECT_DOUBLE_EQ(-2, control);
+}
