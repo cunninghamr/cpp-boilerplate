950176b92529817f03caaf7c0dab1aa6248e3002
Varun Asthana <varunasthana92@gmail.com>
2019-09-26 22:17:01 -0400

    cpplint & cppcheck errors corrected

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,15 @@
+/**
+ * @author Ryan Cunningham
+ * @author Pablo Sanhueza
+ * @author Varun Asthana
+ * @author Aman Virmani
+ *
+ * @file main.cpp
+ * @brief main.cpp
+ *
+ * Copyright [2019] Group12
+ */
+
 #include <iostream>
 
 int main() {
